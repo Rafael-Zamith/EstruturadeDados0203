8ad582b59025168f2ff79528b708603bacc3ebf4
Abner Santos de Queiroz <abner.queiroz@outlook.com>
2020-03-02 10:02:05 -0300

    Update Gamer.java

--- Gamer/src/Gamer/Gamer.java
+++ Gamer/src/Gamer/Gamer.java
@@ -1,5 +1,12 @@
 package Gamer;
 public class Gamer {
     private String name;
-    
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+    public int getScore() {
+        return score;
+    }
 }
