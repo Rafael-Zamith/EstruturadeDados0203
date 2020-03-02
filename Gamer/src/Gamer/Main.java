5af47c7f45a825b63d8ca656e52c01e86773acb6
Rafael-Zamith <31914454@mackenzista.com.br>
2020-03-02 10:30:43 -0300

    Update Main.java

--- Gamer/src/Gamer/Main.java
+++ Gamer/src/Gamer/Main.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-       List<Gamer> gamer;
-       gamer = new ArrayList<Gamer>();
        Vetor v = new Vetor(10);
     }
 }
