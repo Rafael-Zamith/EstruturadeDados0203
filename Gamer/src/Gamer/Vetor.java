ba5c21e5dbb59ad1729010da7e33fe69eb4c61bb
Rafael-Zamith <31914454@mackenzista.com.br>
2020-03-02 10:06:16 -0300

    1.0

---
+++ Gamer/src/Gamer/Vetor.java
@@ -0,0 +1,87 @@
+package Gamer;
+
+/**
+ * Vetor
+ */
+public class Vetor {
+    private int[] A; // armazena os elementos do vetor
+        private int capacity; // capacidade do vetor
+        private int size; // elementos no vetor
+
+        public Vetor(int capacity) {
+         A = new int[capacity];
+        this.size = 0;
+        this.capacity = capacity;
+        }
+        public boolean isEmpty() {
+            //return (size == 0)
+            if (size == 0)
+            return true;
+            else 
+            return false;
+        }
+        public int size() {
+            return size;
+        }
+
+        public int get(int i) throws Exception {
+            if (i>=size)
+            {throw new Exception("Posição inválida para consulta!");
+        }
+
+            return A[i];
+
+        }
+
+        public int set(int i, int n) throws Exception {
+            if (i>=size)
+            {throw new Exception("Posição inválida para consulta!");
+        }
+        A[i]=n;
+        return n;
+        }
+
+        public void add(int i, int n) throws Exception {
+            if(size==A.length){
+                throw new Exception("Lista cheia");
+            }
+            if (i>size)
+            {throw new Exception("Posição inválida!");
+        }
+        for(int j = size -1;j >= i;j--){
+            A[j +1] = A[j];
+        }
+        A[i]=n;
+        size++;
+        }
+
+        public void remove(int i) throws Exception {
+            if (isEmpty()){
+                throw new Exception("Lista Vazia! Impossivel remover");
+            }
+            if(i>size){
+                throw new Exception("Posição Inválida para Remoção");
+            }
+            for(int j =i; j<=size-1;j++){
+                A[j]=A[j+1];
+            }
+            size--;
+        }
+
+        public int search(int n) throws Exception {
+            if(isEmpty())
+            throw new Exception("Lista vazia");
+
+            for(int i=0;i<size;i++)
+                if(A[i]==n)
+                    return i;
+
+            return -1;
+        }
+    
+
+        public int [ ] getArray() {
+         return A;
+        }
+    
+}
